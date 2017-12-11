cbae1d395f75f0d5473c171ef5283b13979ea818
dudy <chenkai1792463586@gmail.com>
2017-12-11 11:08:33 +0800

    add article controller

--- src/main/java/com/ck/controller/admin/ArticleController.java
+++ src/main/java/com/ck/controller/admin/ArticleController.java
@@ -6,6 +6,7 @@ import com.ck.utils.PageEntity;
 import com.ck.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,19 @@ public class ArticleController {
         PageEntity<Contents> pageEntity = articleService.findPage(pageable);
         return Result.ok(pageEntity);
     }
+
+    @RequestMapping(value = "/publish",method = RequestMethod.POST)
+    public Result<String> add(Contents contents){
+        articleService.publish(contents);
+        return Result.ok("文章保存成功");
+    }
+
+    @RequestMapping(value = "/{cid}",method = RequestMethod.GET)
+    public Result<Contents> getContents(@PathVariable("cid") String cid){
+        Contents contents = articleService.getContents(cid).orElse(null);
+        return Result.ok(contents);
+    }
+
+
 
 }
